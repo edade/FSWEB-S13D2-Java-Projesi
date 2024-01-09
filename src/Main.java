66644792d276a8ce563850d66abc14368f4aba8b
Eda Kalaycioglu <edakalaycioglu@gmail.com>
2024-01-09 16:07:51 +0300

    ispalindrome method is written

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println(isPalindrome(-1221));  // true
+        System.out.println(isPalindrome(707));    // true
+        System.out.println(isPalindrome(11212));  // false
+    }
+
+    public static boolean isPalindrome(int number) {
+        number = Math.abs(number);
+        String strNumber = Integer.toString(number);
+        int length = strNumber.length();
+
+        for (int i = 0; i < length / 2; i++) {
+            if (strNumber.charAt(i) != strNumber.charAt(length - i - 1)) {
+                return false;
+            }
+        }
+
+        return true;
+
     }
 }
